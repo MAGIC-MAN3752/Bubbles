4c5a21347d8e68578e0376ef7d1036fcc4ff8b4e
MAGIC-MAN3752 <163941075+MAGIC-MAN3752@users.noreply.github.com>
2025-01-13 17:02:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-function search() {
-    const query = document.getElementById('search-input').value;
-    alert(`You searched for: ${query}`);
-}
+// Make images draggable
+const draggables = document.querySelectorAll('.movable');
+
+draggables.forEach(item => {
+    item.addEventListener('mousedown', (e) => {
+        const element = e.target;
+        let offsetX = e.clientX - element.offsetLeft;
+        let offsetY = e.clientY - element.offsetTop;
+
+        const moveAt = (pageX, pageY) => {
+            element.style.left = pageX - offsetX + 'px';
+            element.style.top = pageY - offsetY + 'px';
+        };
+
+        const onMouseMove = (event) => {
+            moveAt(event.pageX, event.pageY);
+        };
+
+        document.addEventListener('mousemove', onMouseMove);
+
+        document.addEventListener('mouseup', () => {
+            document.removeEventListener('mousemove', onMouseMove);
+        }, { once: true });
+    });
+});
